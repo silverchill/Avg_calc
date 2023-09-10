7bcf784c80237b7b7e271df1fa378c1344957d7d
EMarvelM <marvelousegbe19@gmail.com>
2023-09-10 13:41:01 +0100

    Update script to check for root permissions before copying file

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 source_file="./avg_cal"
 destination_dir="/bin/"
 
+# Check if the user has sufficient permissions
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Error: This script requires root (sudo) permissions to copy files to $destination_dir."
+    exit 1
+fi
+
 # Copy the file to /bin if it doesn't already exist there
 if [ ! -e "$destination_dir/avg_cal" ]; then
     sudo cp "$source_file" "$destination_dir"
